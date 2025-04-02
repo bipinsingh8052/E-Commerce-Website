30d090423f8f16448a3bde508e035db71c6c53de
Bipin Singh <bipinsingh8052@gmail.com>
2025-04-02 22:19:57 +0530

    noraml

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import HeaderCus from './pages/HeaderCus';
+import Navbar from './component/Navbar';
 
 export default function Layout() {
-    let nav=useNavigate();
-    const goit=()=>{
-        nav("/adminlogin")
-    }
+    // let nav=useNavigate();
+    // const goit=()=>{
+    //     nav("/adminlogin")
+    // }
   return (
-    <div>
-      <HeaderCus/>
-      <button onClick={goit}>
+    <>
+      <Navbar/>
+      {/* <HeaderCus/> */}
+      {/* <hr /> */}
+      {/* <button onClick={goit}>
         admin
-      </button>
-    </div>
+      </button> */}
+    </>
   )
 }
--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import '../css/mainNavbar.css'
 export default function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className='navbar'>
+      <div className="NavbarUpperSection">
+        <div className="navbar_innerSection">
+            <div class="logo">
+              <h1> <span class="fir">BIPIN</span>  <span class="secon">STORE</span></h1>
+             </div>
+             <div className="AllOption">
+              lorem
+             </div>
+        </div>
+      </div>
+      <div className='navbar_Main_Section'>
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore repellat quam aut incidunt dolore nesciunt? Corporis dolorem officiis illum doloremque distinctio doloribus est rem soluta voluptate eius. Eos autem neque natus at reiciendis. Unde explicabo minima rerum voluptates quas sint accusantium, molestias sunt? Nihil totam quasi eius, id quo ipsa ratione, ea enim ab rerum molestias nulla sit neque, minus pariatur necessitatibus labore nisi itaque. Libero odit odio, corporis optio enim magnam delectus, esse aliquam ex placeat molestiae hic eligendi quas similique a. Nisi sed consequuntur repudiandae est quam.
+      </div>
     </div>
   )
 }
